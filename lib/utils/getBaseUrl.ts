8c5f6a0e678d767607dd880fb7dff08dadd3949b
Carlos Mayora Curzio <carlos.mayora.curzio@gmail.com>
2023-09-24 13:28:04 +0200

    Revert "Debug getBaseUrl"
    This reverts commit df8eff51436d02a593b87f6633487d449ad8bf3d.

--- lib/utils/getBaseUrl.ts
+++ lib/utils/getBaseUrl.ts
@@ -1,13 +1,8 @@
 export function getBaseUrl(): string {
-  console.log(process.env.NEXT_PUBLIC_SITE_URL);
-  console.log(process.env.NEXT_PUBLIC_VERCEL_URL);
-
   let url =
     process.env.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
     process.env.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
     "http://localhost:3000";
-
-  console.log(url);
 
   // Make sure to include `https://` when not localhost.
   url = url.startsWith("http") ? url : `https://${url}`;
